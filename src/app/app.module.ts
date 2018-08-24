0699f933630e7fea97dd17db215bb37093188afe
Octubrus <augustinasv@gmail.com>
2018-08-24 22:50:18 +0100

    added placeholders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { MethodComponent } from './method/method.component';
+import { ProductsComponent } from './products/products.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/method', pathMatch: 'full' },
+  { path: 'method', component: MethodComponent },
+  { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MethodComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   schemas: [ NO_ERRORS_SCHEMA],
   providers: [],
