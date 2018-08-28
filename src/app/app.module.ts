887b0e184852138351b1accafc3e80341171384c
Octubrus <augustinasv@gmail.com>
2018-08-28 19:54:02 +0100

    lots of text added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { MethodComponent } from './method/method.component';
-import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/method', pathMatch: 'full' },
   { path: 'method', component: MethodComponent },
-  { path: 'products', component: ProductsComponent }
+  { path: 'books', component: BooksComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     MethodComponent,
-    ProductsComponent
+    ContactComponent,
+    AboutComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
