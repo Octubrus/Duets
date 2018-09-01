00d5dadaf0ce36b9edfa156a710b96e6275fd9cd
Octubrus <augustinasv@gmail.com>
2018-09-01 12:14:53 +0100

    added modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { BooksComponent } from './books/books.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'method', component: MethodComponent },
   { path: 'books', component: BooksComponent },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,18 +10,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    /* Sample function that returns boolean in case the browser is Internet Explorer*/
-    function isIE() {
-      let ua = navigator.userAgent;
-      /* MSIE used to detect old browsers and Trident used to newer ones*/
-      var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-
-      return is_ie;
-    }
-    /* Create an alert to show if the browser is IE or not */
-    if (isIE()) {
-      alert('Divertimento Duets is currently not compadible with Internet Explorer. Plese use any other browser such as Google Chrome or Safari.');
-    } 
+    
   }
 
 }
